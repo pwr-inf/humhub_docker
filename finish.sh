19ed4adb1e561191421ccd5a881c6c5d3f82daf9
Piotr Szymanski <niedakh@gmail.com>
2016-09-06 12:12:37 +0200

    unicode database

--- finish.sh
+++ finish.sh
@@ -3,4 +3,5 @@ cp /common.php /app/protected/config/
 cp /Engine.php /app/protected/humhub/modules/user/authclient/
 cp /app/.htaccess{.dist,}
 sed -i -e "s#/user/auth/login#/user/auth/external?authclient=engine#g" /app/protected/humhub/config/web.php
-mysqladmin create humhub
+mysql -e "DROP DATABASE IF EXISTS \`humhub\`"
+mysql -e "CREATE DATABASE \`humhub\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
