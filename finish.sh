2b3b15db98ef07220d1b2f8f559ccbb507d3b7e0
Piotr Szymanski <niedakh@gmail.com>
2016-09-19 14:32:17 +0200

    don't drop db, create db only if does not exist

--- finish.sh
+++ finish.sh
@@ -3,5 +3,4 @@ cp /common.php /app/protected/config/
 cp /Engine.php /app/protected/humhub/modules/user/authclient/
 cp /app/.htaccess{.dist,}
 sed -i -e "s#/user/auth/login#/user/auth/external?authclient=engine#g" /app/protected/humhub/config/web.php
-mysql -e "DROP DATABASE IF EXISTS \`humhub\`"
-mysql -e "CREATE DATABASE \`humhub\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`humhub\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
